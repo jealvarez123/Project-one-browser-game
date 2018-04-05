2ea230925dbb24292c6dc60d9791d95aeb4d602c
jealvarez123 <jealvarez123@gmail.com>
2018-04-05 18:35:50 -0500

    added movement to the ymGamePiece

--- gameFiles/js/app.js
+++ gameFiles/js/app.js
@@ -1,11 +1,71 @@
 console.log('Can I kick it?');
 
-let canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-ctx.fillStyle = '#ff0000';
-ctx.fillRect(0,0,75,75);
+// const canvas = document.getElementById('myCanvas');
+
+// //this is the player
+// var ctx = canvas.getContext('2d');
+// ctx.fillStyle = '#ff0000';
+// ctx.fillRect(1,1,20,20);
+
+// //This is the exit
+// var ctx2 = canvas.getContext('2d');
+// ctx.fillStyle = '#000000';
+// ctx.fillRect(180,180,20,20);
+//
+// //These are the walls
+// var wall = canvas.getContext('2d');
+// ctx.moveTo(25,0);
+// ctx.lineTo(25,50);
+// ctx.stroke();
+//
+// var wall = canvas.getContext('2d');
+// ctx.moveTo(25,50);
+// ctx.lineTo(50,50);
+// ctx.stroke();
+//
+// var wall = canvas.getContext('2d');
+// ctx.moveTo(0,75);
+// ctx.lineTo(30,75);
+// ctx.stroke();
+// var myGamePiece;
+//
 
 
-var ctx2 = canvas.getContext('2d');
-ctx.fillStyle = '#000000';
-ctx.fillRect(125,125,75,75);
+var myGamePiece;
+
+function startGame() {
+    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.x += 1;
+    myGamePiece.update();
+}
