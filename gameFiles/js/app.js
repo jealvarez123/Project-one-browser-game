def3a9bd74e5a548dada2a5e396e04948daf88ea
jealvarez123 <jealvarez123@gmail.com>
2018-04-05 16:02:04 -0500

    created the board with the player and the exit door

--- gameFiles/js/app.js
+++ gameFiles/js/app.js
@@ -1,1 +1,11 @@
 console.log('Can I kick it?');
+
+let canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = '#ff0000';
+ctx.fillRect(0,0,75,75);
+
+
+var ctx2 = canvas.getContext('2d');
+ctx.fillStyle = '#000000';
+ctx.fillRect(125,125,75,75);
