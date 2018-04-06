00ebb4c85b231f2cdf7b4ddb1429151d3ecdeb2c
jealvarez123 <jealvarez123@gmail.com>
2018-04-05 21:30:28 -0500

    added movement with keyboard

--- gameFiles/js/app.js
+++ gameFiles/js/app.js
@@ -1,17 +1,6 @@
 console.log('Can I kick it?');
 
-// const canvas = document.getElementById('myCanvas');
 
-// //this is the player
-// var ctx = canvas.getContext('2d');
-// ctx.fillStyle = '#ff0000';
-// ctx.fillRect(1,1,20,20);
-
-// //This is the exit
-// var ctx2 = canvas.getContext('2d');
-// ctx.fillStyle = '#000000';
-// ctx.fillRect(180,180,20,20);
-//
 // //These are the walls
 // var wall = canvas.getContext('2d');
 // ctx.moveTo(25,0);
@@ -31,14 +20,20 @@ console.log('Can I kick it?');
 //
 
 
-var redGamePiece, blackGamePiece;
+// This stores the game pieces
+var myGamePiece, blackGamePiece;
 
+
+
+//This makes a call to creates the board and pieces
 function startGame() {
-    redGamePiece = new component(20, 20, "red", 10, 120);
-    blackGamePiece = new component(20, 20, "black", 480, 120);
+    myGamePiece = new component(20, 20, "red", 10, 120);
+    // blackGamePiece = new component(20, 20, "black", 480, 120);
     myGameArea.start();
 }
 
+
+// this creates the board and also adds the frame refresher
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -47,27 +42,49 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+          myGameArea.key = e.keyCode;
+      })
+      window.addEventListener('keyup', function (e) {
+          myGameArea.key = false;
+      })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
+// this creates the pieces
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
+    // this.keyboarder = new keyboarder();
     this.update = function(){
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
 
+// this updates the board, which is just a refresh rate. This also affects piece movement
 function updateGameArea() {
-    myGameArea.clear();
-    redGamePiece.x += 1;
-    redGamePiece.update();
-    blackGamePiece.update();
+  myGameArea.clear();
+  myGamePiece.speedX = 0;
+  myGamePiece.speedY = 0;
+      if (myGameArea.key && myGameArea.key == 65) {myGamePiece.speedX = -1; }
+      if (myGameArea.key && myGameArea.key == 68) {myGamePiece.speedX = 1; }
+      if (myGameArea.key && myGameArea.key == 87) {myGamePiece.speedY = -1; }
+      if (myGameArea.key && myGameArea.key == 83) {myGamePiece.speedY = 1; }
+  myGamePiece.update();
+  // blackGamePiece.update();
+  myGamePiece.newPos();
+  myGamePiece.update();
 }
